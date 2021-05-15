37c8c8b9c6398a5be8182ffe09e16021e8b688fe
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-05-15 18:21:20 +0100

    feat: update alert api routes

--- routes/api.php
+++ routes/api.php
@@ -19,20 +19,33 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 /**
-**Basic Routes for a RESTful service:
-**Route::get($uri, $callback);
-**Route::post($uri, $callback);
-**Route::put($uri, $callback);
-**Route::delete($uri, $callback);
-**
-*/
+ **Basic Routes for a RESTful service:
+ **Route::get($uri, $callback);
+ **Route::post($uri, $callback);
+ **Route::put($uri, $callback);
+ **Route::delete($uri, $callback);
+ **
+ */
 
+
+// item routes
 Route::get('items', 'ItemsController@index');
- 
+
 Route::get('items/{item}', 'ItemsController@show');
- 
-Route::post('items','ItemsController@store');
- 
-Route::put('items/{item}','ItemsController@update');
- 
+
+Route::post('items', 'ItemsController@store');
+
+Route::put('items/{item}', 'ItemsController@update');
+
 Route::delete('items/{item}', 'ItemsController@delete');
+
+//alett routes
+Route::get('alerts', 'AlertsController@index');
+
+Route::get('alerts/{alert}', 'AlertsController@show');
+
+Route::post('alerts', 'AlertsController@store');
+
+Route::put('alerts/{alert}', 'AlertsController@update');
+
+Route::delete('alerts/{alert}', 'AlertsController@delete');

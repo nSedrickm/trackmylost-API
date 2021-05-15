6342af85047a0fe74cbfb6beb2bfa4178f2262f5
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-05-15 18:13:03 +0100

    feat: create alert model and migration

---
+++ app/Models/Alert.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Alert extends Model
+{
+    use HasFactory;
+    protected $fillable = ['name', 'document_type', 'email','phone_number'];
+}
---
+++ database/migrations/2021_05_15_171017_create_alerts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlertsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alerts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('document_type');
+            $table->string('email');
+            $table->string('phone_number');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alerts');
+    }
+}

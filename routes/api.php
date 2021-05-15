afcfd7371e458039d27d5ed84c61873c26e72271
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-05-15 15:08:33 +0100

    chore: generate item model and migration

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,38 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/**
+ ** Basic Routes for a RESTful service:
+ **
+ ** Route::get($uri, $callback);
+ ** Route::post($uri, $callback);
+ ** Route::put($uri, $callback);
+ ** Route::delete($uri, $callback);
+ **
+ **/
+
+Route::get('products', function () {
+    return response(['Product 1', 'Product 2', 'Product 3'], 200);
+});
+
+Route::get('products/{product}', function ($productId) {
+    return response()->json(['productId' => "{$productId}"], 200);
+});
+
+
+Route::post('products', function () {
+    return  response()->json([
+        'message' => 'Create success'
+    ], 201);
+});
+
+Route::put('products/{product}', function () {
+    return  response()->json([
+        'message' => 'Update success'
+    ], 200);
+});
+
+Route::delete('products/{product}', function () {
+    return  response()->json(null, 204);
+});

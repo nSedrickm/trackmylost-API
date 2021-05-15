b8a6c7fedbce33e3524479c44e0b2c2af455b553
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-05-15 18:18:50 +0100

    feat: create alert controller

---
+++ app/Http/Controllers/AlertsController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Alert;
+
+class AlertsController extends Controller
+{
+    public function index()
+    {
+        return Alert::all();
+    }
+
+    public function show(Alert $alert)
+    {
+        return $alert;
+    }
+
+    public function store(Request $request)
+    {
+        $alert = Alert::create($request->all());
+
+        return response()->json($alert, 201);
+    }
+
+    public function update(Request $request, Alert $alert)
+    {
+        $alert->update($request->all());
+
+        return response()->json($alert, 200);
+    }
+
+    public function delete(Alert $alert)
+    {
+        $alert->delete();
+
+        return response()->json(null, 204);
+    }
+}

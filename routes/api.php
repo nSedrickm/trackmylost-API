48bebf26cc81f1f74eb08f289693d982cc89ffdb
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-05-15 16:22:49 +0100

    update: add Item controller and update routes

--- routes/api.php
+++ routes/api.php
@@ -19,36 +19,20 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 /**
- ** Basic Routes for a RESTful service:
- **
- ** Route::get($uri, $callback);
- ** Route::post($uri, $callback);
- ** Route::put($uri, $callback);
- ** Route::delete($uri, $callback);
- **
- **/
+**Basic Routes for a RESTful service:
+**Route::get($uri, $callback);
+**Route::post($uri, $callback);
+**Route::put($uri, $callback);
+**Route::delete($uri, $callback);
+**
+*/
 
-Route::get('products', function () {
-    return response(['Product 1', 'Product 2', 'Product 3'], 200);
-});
-
-Route::get('products/{product}', function ($productId) {
-    return response()->json(['productId' => "{$productId}"], 200);
-});
-
-
-Route::post('products', function () {
-    return  response()->json([
-        'message' => 'Create success'
-    ], 201);
-});
-
-Route::put('products/{product}', function () {
-    return  response()->json([
-        'message' => 'Update success'
-    ], 200);
-});
-
-Route::delete('products/{product}', function () {
-    return  response()->json(null, 204);
-});
+Route::get('items', 'ItemsController@index');
+ 
+Route::get('items/{item}', 'ItemsController@show');
+ 
+Route::post('items','ItemsController@store');
+ 
+Route::put('items/{item}','ItemsController@update');
+ 
+Route::delete('items/{item}', 'ItemsController@delete');

65704711b5915c55d627d3b478daeb73115d31de
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-05-22 13:29:23 +0100

    feat: add user controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        $this->validator($request->all())->validate();
+        $user = $this->create($request->all());
+        $this->guard()->login($user);
+        return response()->json([
+            'user' => $user,
+            'message' => 'registration successful'
+        ], 200);
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'phone_number' => ['required', 'string', 'max:255', 'unique:users'],
+            'town' => ['required', 'string', 'max:255'],
+            'password' => ['required', 'string', 'min:6'],
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration
+     * 
+     * @param array $data
+     * @return \App\Models\User
+     */
+
+    protected function create(array $data)
+    {
+
+        return User::create([
+            'first_name' => $data['first_name'],
+            'last_name' => $data['last_name'],
+            'phone_number' => $data['phone_number'],
+            'town' => $data['town'],
+            'password' => Hash::make($data['password']),
+        ]);
+    }
+
+    protected function guard()
+    {
+        return Auth::guard();
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('phone_number', 'password');
+
+        if (Auth::attempt($credentials)) {
+            // auth passed
+            $authuser = auth()->user();
+            return response()->json(['message' => 'Login successful'], 200);
+        } else {
+            return response()->json(['message' => 'Invalid phone number or password'], 401);
+        }
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return response()->json(['message' => 'Logged Out'], 200);
+    }
+}

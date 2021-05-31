579d2d44c8ece0fb158f6ae08d4f391535b9c853
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-05-31 12:52:03 +0100

    update: add user guard

--- config/auth.php
+++ config/auth.php
@@ -40,11 +40,14 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
-
         'api' => [
             'driver' => 'token',
             'provider' => 'users',
             'hash' => false,
+        ],
+        'user' => [
+            'driver' => 'session',
+            'provider' => 'users',
         ],
         'admin' => [
             'driver' => 'session',

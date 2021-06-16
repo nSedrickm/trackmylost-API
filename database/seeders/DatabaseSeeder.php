6ee5c3a875021f994efed79f978964cd30731228
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-06-16 10:38:50 +0100

    update: seed db at once for all tables

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call([
             AlertsTableSeeder::class,
-            ItemsTableSeeder::class
+            ItemsTableSeeder::class,
+            NotificationsTableSeeder::class
         ]);
     }
 }

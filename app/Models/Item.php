f7f38e9378e519f8dac79a187ca1bcb6948d1765
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-06-16 16:01:36 +0100

    refactor: add accessor to get rull names for each model

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -9,4 +9,14 @@ class Item extends Model
 {
     use HasFactory;
     protected $fillable = ['document_type', 'first_name', 'other_names', 'phone_number', 'reward'];
+
+    /**
+     * Get the full name.
+     *
+     * @return string
+     */
+    public function getFullName()
+    {
+        return "{$this->first_name} {$this->last_name}";
+    }
 }
